3540e1cbee9236bcb911e6d27f9159018c7899a3
Jan Mack <jan.mack@de.schunk.com>
2025-06-24 10:29:19 +0200

    Adapt scanner error handling and remove unnecessary clear_buffer function

--- schunk_gripper_library/schunk_gripper_library/tests/conftest.py
+++ schunk_gripper_library/schunk_gripper_library/tests/conftest.py
@@ -6,14 +6,7 @@ import pymodbus
 from .etc.modbus_server import ModbusServer
 import asyncio
 import threading
-import time
-from .etc.scanner_helper import stop_all, start_bks_simulation
-
-
-def pytest_collection_modifyitems(items):
-    for item in items:
-        if not item.get_closest_marker("no_timeout"):
-            item.add_marker(pytest.mark.timeout(30))
+from .etc.scanner_helper import stop_all, start_bks_simulation, stop_bks_simulation
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -22,30 +15,14 @@ def bks_simulation(request):
     Automatically start a BKS simulation for each test module and clean up after.
     This fixture runs automatically for every test file.
     """
-    if request.node.get_closest_marker("no_bks"):
+    if request.module.__name__.endswith("test_scanner"):
         yield
         return
     # Start simulation with unique ID based on module
     start_bks_simulation(sim_id=12, serial_num="00000012", device_index=4)
     yield
     # Clean up after all tests in the module are complete
-    stop_all()
-
-
-@pytest.fixture
-def time_scan():
-    """
-    Fixture to measure the execution time of the scanner.scan function.
-    """
-
-    def measure_scan_time(scanner, *args, **kwargs):
-        start_time = time.time()
-        result = asyncio.run(scanner.scan(*args, **kwargs))
-        end_time = time.time()
-        execution_time = end_time - start_time
-        return result, execution_time
-
-    return measure_scan_time
+    stop_bks_simulation(sim_id=12)
 
 
 @pytest.fixture(scope="module")
@@ -100,6 +77,4 @@ def simulate_pymodbus_failure():
 
 @pytest.fixture()
 def cleanup():
-    assert (
-        stop_all() is None
-    ), "Failed to stop all scanners and drivers after test execution."
+    stop_all()
