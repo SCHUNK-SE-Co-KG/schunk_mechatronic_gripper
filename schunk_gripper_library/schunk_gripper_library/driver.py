98c207b09b5373fab66cc04b40f92372a4b689db
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-08-29 17:43:05 +0200

    Always read the command received bit as first step
    See previous commit on `misconception about command received bit`.

--- schunk_gripper_library/schunk_gripper_library/driver.py
+++ schunk_gripper_library/schunk_gripper_library/driver.py
@@ -234,9 +234,9 @@ class Driver(object):
             return False
 
         def do() -> bool:
+            cmd_toggle_before = self.get_status_bit(bit=5)
             self.clear_plc_output()
             self.send_plc_output()
-            cmd_toggle_before = self.get_status_bit(bit=5)
             self.set_control_bit(bit=2, value=True)
             self.send_plc_output()
             desired_bits = {"0": 1, "5": cmd_toggle_before ^ 1}
@@ -252,9 +252,9 @@ class Driver(object):
             return False
 
         def do() -> bool:
+            cmd_toggle_before = self.get_status_bit(bit=5)
             self.clear_plc_output()
             self.send_plc_output()
-            cmd_toggle_before = self.get_status_bit(bit=5)
             self.set_control_bit(
                 bit=0, value=False
             )  # activate fast stop (inverted behavior)
@@ -271,10 +271,9 @@ class Driver(object):
         if not self.connected:
             return False
 
+        cmd_toggle_before = self.get_status_bit(bit=5)
         self.clear_plc_output()
         self.send_plc_output()
-
-        cmd_toggle_before = self.get_status_bit(bit=5)
         self.set_control_bit(bit=1, value=True)
         self.send_plc_output()
 
@@ -296,9 +295,9 @@ class Driver(object):
             return False
 
         def start():
+            cmd_toggle_before = self.get_status_bit(bit=5)
             self.clear_plc_output()
             self.send_plc_output()
-            cmd_toggle_before = self.get_status_bit(bit=5)
             self.set_control_bit(bit=13, value=True)
             if self.gpe_available():
                 self.set_control_bit(bit=31, value=use_gpe)
@@ -334,10 +333,9 @@ class Driver(object):
         if not self.connected:
             return False
 
+        cmd_toggle_before = self.get_status_bit(bit=5)
         self.clear_plc_output()
         self.send_plc_output()
-
-        cmd_toggle_before = self.get_status_bit(bit=5)
         self.set_control_bit(bit=14, value=True)
         self.set_control_bit(bit=31, value=use_gpe)
         self.set_target_position(position)
@@ -361,10 +359,9 @@ class Driver(object):
             return False
 
         def start() -> bool:
+            cmd_toggle_before = self.get_status_bit(bit=5)
             self.clear_plc_output()
             self.send_plc_output()
-
-            cmd_toggle_before = self.get_status_bit(bit=5)
             self.set_control_bit(bit=12, value=True)
             self.set_control_bit(bit=7, value=outward)
             if self.gpe_available():
@@ -402,9 +399,9 @@ class Driver(object):
             return False
 
         def start() -> bool:
+            cmd_toggle_before = self.get_status_bit(bit=5)
             self.clear_plc_output()
             self.send_plc_output()
-            cmd_toggle_before = self.get_status_bit(bit=5)
             self.set_control_bit(bit=11, value=True)
             if self.gpe_available():
                 self.set_control_bit(bit=31, value=use_gpe)
