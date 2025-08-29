f5767ce579a319450311a913e7f983caef73655c
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2025-08-29 18:21:39 +0200

    Use a `Setup` class for jogging tests
    This makes the tests more readable.

--- schunk_gripper_library/schunk_gripper_library/tests/test_jogging.py
+++ schunk_gripper_library/schunk_gripper_library/tests/test_jogging.py
@@ -3,9 +3,24 @@ from schunk_gripper_library.driver import Driver
 import time
 
 
+class Setup(object):
+    def __init__(self, driver: Driver):
+        self.driver = driver
+
+    def reset(self) -> bool:
+        if not self.driver.connected:
+            return False
+        max_pos = self.driver.module_parameters["max_pos"]
+        min_pos = self.driver.module_parameters["min_pos"]
+        self.max_vel = self.driver.module_parameters["max_vel"]
+        self.middle = int(0.5 * (max_pos - min_pos))
+        return self.driver.acknowledge() and self.driver.move_to_absolute_position(
+            position=self.middle, velocity=self.max_vel
+        )
+
+
 @skip_without_gripper
 def test_driver_offers_start_and_stop_jogging():
-
     driver = Driver()
 
     # When not connected
@@ -14,28 +29,20 @@ def test_driver_offers_start_and_stop_jogging():
 
     # Setup
     assert driver.connect(serial_port="/dev/ttyUSB0", device_id=12)
-    max_pos = driver.module_parameters["max_pos"]
-    min_pos = driver.module_parameters["min_pos"]
-    max_vel = driver.module_parameters["max_vel"]
-    middle = int(0.5 * (max_pos - min_pos))
-
-    def reset() -> bool:
-        return driver.acknowledge() and driver.move_to_absolute_position(
-            position=middle, velocity=max_vel
-        )
+    setup = Setup(driver)
 
     # Positive jogging
-    assert reset()
-    assert driver.start_jogging(+max_vel)
+    assert setup.reset()
+    assert driver.start_jogging(+setup.max_vel)
     time.sleep(0.5)
-    assert driver.get_actual_position() > middle
+    assert driver.get_actual_position() > setup.middle
     assert driver.stop_jogging()
 
     # Negative jogging
-    assert reset()
-    assert driver.start_jogging(-max_vel)
+    assert setup.reset()
+    assert driver.start_jogging(-setup.max_vel)
     time.sleep(0.5)
-    assert driver.get_actual_position() < middle
+    assert driver.get_actual_position() < setup.middle
     assert driver.stop_jogging()
 
     # Cleanup
